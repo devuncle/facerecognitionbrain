d7f364e21f9bc2452ea137b5bc3f57ad26ff0bcb
devuncle <devuncle.com@gmail.com>
2018-04-18 16:07:48 +0800

    add navigation

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import tachyans;
+import './App.css';
+import Navigation from '../components/Navigation/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navigation />
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
