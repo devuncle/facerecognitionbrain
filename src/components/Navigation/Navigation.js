4f284a894e5b757be3054c7241aef69a3966d869
devuncle <devuncle.com@gmail.com>
2018-04-21 11:06:39 +0800

    finish register

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const Navigation = ({onRouteChange}) => {
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
     return (
-        <nav className='' style={{display: 'flex', justifyContent: 'flex-end'}}  >
-            <p onClick={()=> onRouteChange('signin')} className='f3 link dim red underline pa3 pointer'>Sign Out</p>
-        </nav>
+      <nav className="" style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim red underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
     );
-}
+  } else {
+    return (
+      <nav className="" style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim red underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange("Register")}
+          className="f3 link dim red underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
+};
 
 export default Navigation;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,68 +1,72 @@
-import React, { Component } from 'react';
-import './App.css';
-import Clarifai from 'clarifai';
-import Navigation from '../components/Navigation/Navigation';
-import Logo from '../components/Logo/Logo';
-import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
-import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
-import Rank from '../components/Rank/Rank';
-import Particles from 'react-particles-js';
-import SignIn from '../components/SignIn/SignIn';
+import React, { Component } from "react";
+import "./App.css";
+import Clarifai from "clarifai";
+import Navigation from "../components/Navigation/Navigation";
+import Logo from "../components/Logo/Logo";
+import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "../components/FaceRecognition/FaceRecognition";
+import Rank from "../components/Rank/Rank";
+import Particles from "react-particles-js";
+import SignIn from "../components/SignIn/SignIn";
+import Register from "../components/Register/Register";
 
 const app = new Clarifai.App({
-  apiKey: 'cc8c9bad87f04bb6adaaed2c050b7511'
- });
-
+  apiKey: "cc8c9bad87f04bb6adaaed2c050b7511"
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box:{},
-      route: 'signin'
-    }
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false
+    };
   }
 
-
-  calculatFaceLocation = (data)=>{
+  calculatFaceLocation = data => {
     const clarifaiFace = data.output[0].data.region[0].region_info.bounding_box;
-    const image = document.getElementById('imageinput');
-  }
-  onInputChange = (event) => {
-    this.setState({input: event.target.value});
-    
-  }
+    const image = document.getElementById("imageinput");
+  };
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input}) ;
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(
-    function(response) {
-      // do something with response
-      // console.log(response);
-      this.calculatFaceLocation(response);
-    },
-    function(err) {
-      // there was an error
-      // console.log(err);
-    }
-  );
-  }
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        // console.log(response);
+        this.calculatFaceLocation(response);
+      },
+      function(err) {
+        // there was an error
+        // console.log(err);
+      }
+    );
+  };
 
-  onRouteChange = (route)=>{
-    this.setState({route: route});
-  }
+  onRouteChange = route => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    if (this.state.route !== route) {
+      this.setState({ route: route });
+    }
+  };
+
   render() {
     return (
       <div className="App">
-
-          <Particles className='particales'
+        <Particles
+          className="particales"
           params={{
-
             particles: {
               number: {
                 value: 100
@@ -70,21 +74,29 @@ class App extends Component {
             }
           }}
           style={{
-            width: '100%',
-            backgroundImage: `` 
+            width: "100%",
+            backgroundImage: ``
           }}
         />
-        <Navigation onRouteChange={this.onRouteChange}/>
-        {
-          this.state.route === 'signin' 
-          ? <SignIn onRouteChange={ this.onRouteChange } /> 
-          : <div>
-          <Logo />          
-          <Rank />
-          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition imageUrl={this.state.imageUrl} />
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {this.state.route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={this.state.imageUrl} />
           </div>
-        }
+        ) : this.state.route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
